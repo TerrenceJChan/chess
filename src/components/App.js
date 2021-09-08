81070c3d2ff1c1fdeb05b7277939f26e2c8ac135
Terrence Chan <terrencechan.writer@gmail.com>
2021-09-08 18:40:36 -0400

    Create grid

---
+++ src/components/App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="chessboard">
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+        <div className="chessboard_square">Item</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
