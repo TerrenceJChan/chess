9970c674f14d015dfa3bb940be668afccd5f508f
Terrence Chan <terrencechan.writer@gmail.com>
2021-09-11 01:03:06 -0400

    Algorithmically render chessboard squares

--- src/components/App.js
+++ src/components/App.js
@@ -1,81 +1,29 @@
 import React from 'react';
 import './App.css';
+import parse from 'html-react-parser';
 
 function App() {
+
+  const init = () => {
+    const CHAR_INT = 104;
+    let code = "";
+
+    const squareId = (num, i) => {
+      return String.fromCharCode(CHAR_INT - num) + ((i % 8) + 1);
+    }
+
+    for (let i = 0; i < 64; i++) {
+      let counter = Math.floor(i / 8);
+      code = code + `<div id="${squareId(counter, i)}" className="chessboard_square">Item</div>`;
+    }
+
+    return code;
+  }
+
   return (
     <div className="app">
       <div className="chessboard">
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
-        <div className="chessboard_square">Item</div>
+        {parse(`${init()}`)}
       </div>
     </div>
   );
