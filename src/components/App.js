af9dde06796e90a7deef7ee5e2f9e04438c1fa86
Terrence Chan <terrencechan.writer@gmail.com>
2021-09-13 01:24:45 -0400

    Checker the squares

--- src/components/App.js
+++ src/components/App.js
@@ -12,9 +12,68 @@ function App() {
       return String.fromCharCode(CHAR_INT - num) + ((i % 8) + 1);
     }
 
+    const squareColor = (i) => {
+      const isEven = (i) => {
+        if (i % 2 == 0) {
+          return true
+        } else {
+          return false
+        }
+      }
+      if (i < 8) {
+        if (!isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      } else if (i < 16) {
+        if (isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      } else if (i < 24) {
+        if (!isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      } else if (i < 32) {
+        if (isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      } else if (i < 40) {
+        if (!isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      } else if (i < 48) {
+        if (isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      } else if (i < 56) {
+        if (!isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      } else if (i < 64) {
+        if (isEven(i)) {
+          return "chessboard_square___black"
+        } else {
+          return "chessboard_square___white"
+        }
+      }
+    }
+
     for (let i = 0; i < 64; i++) {
       let counter = Math.floor(i / 8);
-      code = code + `<div id="${squareId(counter, i)}" className="chessboard_square">Item</div>`;
+      code = code + `<div id="${squareId(counter, i)}" className="${["chessboard_square", squareColor(i)].join(" ")}">Item</div>`;
     }
 
     return code;
